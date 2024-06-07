da76c8da7d5d6d0999f53b3f8a087b02ffa1dad3
wasim475 <wasimhossain575@gmail.com>
2024-06-07 13:41:16 +0600

    write delete method for cancel bookings

--- visit-bd-land-server/index.js
+++ visit-bd-land-server/index.js
@@ -53,6 +53,15 @@ async function run() {
         const result = await bookingData.insertOne(bookgData)
         res.send(result)
       })
+
+      app.delete('/wishlist/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await WishlistData.deleteOne(query)
+        res.send(result)
+      })
+
+
     app.get('/packages', async(req, res)=>{
       const cursor = packData.find()
       const result = await cursor.toArray()

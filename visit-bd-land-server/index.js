a207fdad01cb68957c73f419b7c09eefb338d710
wasim475 <wasimhossain575@gmail.com>
2024-06-08 19:42:17 +0600

    added delete method for delete user

--- visit-bd-land-server/index.js
+++ visit-bd-land-server/index.js
@@ -140,6 +140,13 @@ async function run() {
                   res.send(result)
                 })
 
+                app.delete('/users/:id',async(req, res)=>{
+                  const id = req.params.id;
+                  const query = {_id: new ObjectId(id)}
+                  const result = await UserCollection.deleteOne(query)
+                  res.send(result)
+                })
+
  
 
 //     app.get('/userChoice', async (req, res) => {

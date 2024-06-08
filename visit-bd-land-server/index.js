ec1f1e4f1d11ede729f25e545b0c9052321eca4d
wasim475 <wasimhossain575@gmail.com>
2024-06-08 20:06:58 +0600

    write patch method for admin

--- visit-bd-land-server/index.js
+++ visit-bd-land-server/index.js
@@ -147,6 +147,20 @@ async function run() {
                   res.send(result)
                 })
 
+                app.patch('/users/admin/:id',async(req, res)=>{
+                  const id = req.params.id;
+                  const query = {_id: new ObjectId(id)}
+                  const updateDoc = {
+                    $set: {
+                      role: "admin",
+                    }
+                  }
+                  const result = await UserCollection.updateOne(query, updateDoc);
+                  res.send(result)
+                })
+
+
+
  
 
 //     app.get('/userChoice', async (req, res) => {
